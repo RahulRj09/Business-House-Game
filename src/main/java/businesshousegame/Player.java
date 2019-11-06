bc4bfbcf591975379544d25e6e328a390d339727
RahulRj09 <rahul18@navgurukul.org>
2019-11-06 09:29:58 +0530

    Add. getter and setter method in Player class

--- src/main/java/businesshousegame/Player.java
+++ src/main/java/businesshousegame/Player.java
@@ -3,4 +3,20 @@ package businesshousegame;
 public class Player {
     private int amount = 1000;
     private int position = 0;
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
 }
